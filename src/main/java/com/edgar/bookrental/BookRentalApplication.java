12a1f058c10f3c4184e74fc08c94157b9555d62f
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-03-09 22:59:23 -0500

    headers fixed--- cors

--- src/main/java/com/edgar/bookrental/BookRentalApplication.java
+++ src/main/java/com/edgar/bookrental/BookRentalApplication.java
@@ -1,13 +1,34 @@
 package com.edgar.bookrental;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class BookRentalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookRentalApplication.class, args);
+	}
+	
+
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    // Don't do this in production, use a proper list  of allowed origins
+	    config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+	    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Access-Control-Allow-Origin"));
+	    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
 	}
 
 }
--- src/main/java/com/edgar/bookrental/config/SecurityConfiguration.java
+++ src/main/java/com/edgar/bookrental/config/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package com.edgar.bookrental.config;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -12,9 +10,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,20 +23,7 @@ public class SecurityConfiguration {
 	private final LogoutHandler logoutHandler;
 	
 	
-	
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-	  CorsConfiguration configuration = new CorsConfiguration();
-	  configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-	  configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
-	  configuration.setAllowCredentials(true);
-	  configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type"));
-	  configuration.setExposedHeaders(Arrays.asList("X-Get-Header"));
-	  configuration.setMaxAge(3600L);
-	  UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	  source.registerCorsConfiguration("/**", configuration);
-	  return source;
-	}
+
 	
 	
 
@@ -51,6 +33,8 @@ public class SecurityConfiguration {
 		
 		
 		http
+			.cors()
+			.and()
 			.csrf()
 			.disable()
 			.authorizeHttpRequests()
